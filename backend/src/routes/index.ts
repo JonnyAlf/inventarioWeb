f269a950879e8a571358efeb3c15994cc3a24f5c
JonnyAlf <jaleite@minha.fag.edu.br>
2024-11-18 19:50:54 -0300

    adicionado backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Application, Router } from "express";
 import { fornecedorRouter } from "./fornecedorRouter";
 import { usuarioRouters } from "./usuarioRouter";
 import { produtoRouter } from "./produtoRouter";
+import { clienteRouter } from "./clienteRouter";
 
 export const useRoutes = (app: Application) => {
     const apiRouter = Router();
     apiRouter.use('/fornecedor', fornecedorRouter);
     apiRouter.use('/usuario',usuarioRouters);
     apiRouter.use('/produto',produtoRouter);
+    apiRouter.use('/cliente',clienteRouter);
     app.use('/api/v1', apiRouter);
 }
